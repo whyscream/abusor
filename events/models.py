f217e56bf1ec92d6892e596c45cfcbdd93e6972f
Tom Hendrikx <tom@whyscream.net>
2016-03-02 20:37:10 +0100

    Adapt fields

--- events/models.py
+++ events/models.py
@@ -1,13 +1,20 @@
-from django.db import models
+from datetime import datetime
 
+from django.db import models
 
 class Case(models.Model):
     """A collection of related abuse related events."""
     ip_address = models.GenericIPAddressField()
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
-    summary = models.TextField()
+    start_date = models.DateTimeField(default=datetime.now)
+    end_date = models.DateTimeField(blank=True)
+    subject = models.CharField(max_length=128, blank=True)
+    description = models.TextField(blank=True)
 
+    def __str__(self):
+        """Default string representation."""
+        if self.subject:
+            return self.ip_address + ": " + self.subject
+        return self.ip_address + ": " + self.start_date.isoformat()
 
 class Event(models.Model):
     """An abuse related event."""
@@ -23,9 +30,15 @@ class Event(models.Model):
     ip_address = models.GenericIPAddressField()
     date = models.DateTimeField()
     subject = models.CharField(max_length=128)
-    description = models.TextField()
-    score = models.DecimalField(max_digits=5, decimal_places=2, default=None, null=True)
-    category = models.CharField(max_length=10, choices=CATEGORY_CHOICES)
+    description = models.TextField(blank=True)
+    score = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
+    category = models.CharField(max_length=10, choices=CATEGORY_CHOICES, blank=True)
     case = models.ForeignKey('Case', models.SET_NULL, blank=True, null=True)
-    report_date = models.DateTimeField()
-    external_reference = models.CharField(max_length=128)
+    report_date = models.DateTimeField(default=datetime.now)
+    external_reference = models.CharField(max_length=128, blank=True)
+
+    def __str__(self):
+        """Default string representation."""
+        if self.subject:
+            return self.ip_address + ": " + self.subject
+        return self.ip_address + ": " + self.date.isoformat()
