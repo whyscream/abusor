734602765b6e149469c76b001a5fb9bd9a5af830
Tom Hendrikx <tom@whyscream.net>
2017-01-21 00:55:00 +0100

    move tests around, rename files

--- tests/unit/test_events_models.py
+++ tests/unit/test_events_models.py
@@ -7,6 +7,22 @@ from events.models import Case, Event
 NOW = timezone.now()
 
 
+def test_case_str_formatting(random_ipv4, random_ipv6):
+    """Verify default formatting of a Case when cast to string."""
+    case = Case(ip_address=random_ipv4)
+    result = str(case)
+    assert random_ipv4 in result
+    assert timezone.now().strftime("%Y-%m-%d") in result
+
+    case.ip_address = random_ipv6
+    result = str(case)
+    assert random_ipv6 in result
+
+    case.subject = 'foo bar'
+    result = str(case)
+    assert "foo bar (" + random_ipv6 + ")" == result
+
+
 def test_event_str_formatting(random_ipv4, random_ipv6):
     """Verify default formatting of an Event when cast to string."""
     event = Event(ip_address=random_ipv4, subject='foo')
