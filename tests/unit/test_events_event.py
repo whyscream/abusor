3ce19e56faca1bf6842a49c12de8c330bdc18e2f
Tom Hendrikx <tom@whyscream.net>
2017-01-21 00:50:50 +0100

    Add test for finding an existing case for a new event

--- tests/unit/test_events_event.py
+++ tests/unit/test_events_event.py
@@ -1,7 +1,10 @@
 import pytest
 from django.utils import timezone
 
-from events.models import Event
+from events.models import Case, Event
+
+
+NOW = timezone.now()
 
 
 def test_event_str_formatting(random_ipv4, random_ipv6):
@@ -18,7 +21,15 @@ def test_event_str_formatting(random_ipv4, random_ipv6):
 @pytest.mark.django_db
 def test_create_event_creates_case(random_ipv4):
     """Verify that a new Event also creates a new Case."""
-    event = Event.objects.create(ip_address=random_ipv4, date=timezone.now(), subject='foo')
+    event = Event.objects.create(ip_address=random_ipv4, date=NOW, subject='foo')
     assert event.case is not None
     assert event.case.ip_address == random_ipv4
     assert event.case.subject == 'foo'
+
+
+@pytest.mark.django_db
+def test_create_event_finds_existing_case(case):
+    """Cerify that an open Case is connected to a new event when applicable."""
+    event = Event.objects.create(ip_address=case.ip_address, date=NOW, subject='foo')
+    assert event.case == case
+    assert Case.objects.last() == case, "A new case was created when it was not expected"
