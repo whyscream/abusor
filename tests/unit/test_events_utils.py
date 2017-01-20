8b8c1f1b6592014edae054ebd3022053b1e19269
Tom Hendrikx <tom@whyscream.net>
2017-01-21 00:05:21 +0100

    Improve search related case algorithm

--- tests/unit/test_events_utils.py
+++ tests/unit/test_events_utils.py
@@ -22,17 +22,34 @@ def test_find_related_case(event, case_factory):
 
 
 def test_find_related_case_none(event, case_factory, random_ipv6):
-    """Verify that finding a related case will not work when the ip address differs."""
+    """Verify that finding a related Case will not work when the ip address differs."""
     case_factory(ip_address=random_ipv6)
     result = find_related_case(event)
     assert result is None
 
 
 def test_find_related_case_newest(event, case_factory):
-    """Verify that we always find the newest case when multiple results are available."""
-    case1 = case_factory(ip_address=event.ip_address, start_date=YESTERDAY)
-    case2 = case_factory(ip_address=event.ip_address, start_date=NOW)
+    """Verify that we always find the newest Case when multiple results are available."""
+    newest_case = case_factory(ip_address=event.ip_address, start_date=NOW)
+    case_factory(ip_address=event.ip_address, start_date=YESTERDAY)
 
     result = find_related_case(event)
-    assert result != case1
-    assert result == case2
+    assert result == newest_case
+
+
+def test_find_related_case_opened(event, case_factory):
+    """Verify that when both open and closed Cases apply to an Event, the opened one is found."""
+    open_case = case_factory(ip_address=event.ip_address, end_date=None)
+    case_factory(ip_address=event.ip_address, end_date=YESTERDAY)
+
+    result = find_related_case(event)
+    assert result == open_case
+
+
+def test_find_related_case_newest_closed(event, case_factory):
+    """Verify that we find the last closed Case when only closed cases apply to an Event."""
+    last_closed = case_factory(ip_address=event.ip_address, end_date=YESTERDAY)
+    case_factory(ip_address=event.ip_address, end_date=LAST_WEEK)
+
+    result = find_related_case(event)
+    assert result == last_closed
