d18ca40dbdc951510cf2868923afa5aa4f750456
Tom Hendrikx <tom@whyscream.net>
2017-01-21 19:14:38 +0100

    Customize admin a bit

--- events/admin.py
+++ events/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 
 from .models import Event, Case
 
+
+class CaseAdmin(admin.ModelAdmin):
+
+    list_display = ('ip_address', 'subject', 'start_date', 'number_of_events', 'score')
+    list_display_links = ('ip_address', 'subject')
+
+    def number_of_events(self, obj):
+        """The number of events for the case."""
+        return obj.events.count()
+
 admin.site.register(Event)
-admin.site.register(Case)
+admin.site.register(Case, CaseAdmin)
