fc6b5c0037a348a62cba57feb8fa43f4e8dc2f14
Tom Hendrikx <tom@whyscream.net>
2017-01-21 16:02:11 +0100

    Enable token authentication

--- abusor/settings.py
+++ abusor/settings.py
@@ -11,6 +11,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
     'PAGE_SIZE': 10,
 }
 
