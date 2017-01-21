50b06c0e14a22743775a02c1457848ed7b1b300e
Tom Hendrikx <tom@whyscream.net>
2017-01-22 00:44:31 +0100

    cleaning up the event admin

--- events/admin.py
+++ events/admin.py
@@ -3,6 +3,17 @@ from django.contrib import admin
 from .models import Event, Case
 
 
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('ip_address', 'subject', 'date', 'score', 'has_case')
+    list_display_links = ('ip_address', 'subject')
+    readonly_fields = ('report_date',)
+
+    def has_case(self, obj):
+        """Whether a case is attached or not."""
+        return bool(obj.case)
+    has_case.boolean = True
+
+
 class EventInline(admin.TabularInline):
     model = Event
     fields = ('date', 'ip_address', 'subject', 'description', 'score')
@@ -27,5 +38,5 @@ class CaseAdmin(admin.ModelAdmin):
         """The number of events for the case."""
         return obj.events.count()
 
-admin.site.register(Event)
+admin.site.register(Event, EventAdmin)
 admin.site.register(Case, CaseAdmin)
