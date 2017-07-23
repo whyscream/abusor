63fcdc98a7a504c2a0a1700afa91f64d2fc403f0
Tom Hendrikx <tom@whyscream.net>
2017-07-23 15:33:01 +0200

    Limit the fields that are writeable through the api

--- events/serializers.py
+++ events/serializers.py
@@ -9,10 +9,11 @@ class CaseSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Case
         fields = '__all__'
+        read_only_fields = ('as_number', 'country_code', 'end_date', 'score')
 
 
 class EventSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Event
         fields = '__all__'
-        read_only_fields = ('as_number',)
+        read_only_fields = ('as_number', 'country_code', 'report_date', 'score', 'case')
