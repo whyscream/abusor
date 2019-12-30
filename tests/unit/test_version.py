7d8bc31d46de2865a1f7cf403f9861540f44a3ea
Tom Hendrikx <tom@whyscream.net>
2019-12-30 18:02:13 +0100

    Add skip for python 3.6

--- tests/unit/test_version.py
+++ tests/unit/test_version.py
@@ -1,8 +1,14 @@
 import subprocess
+import sys
+
+import pytest
 
 from abusor.version import VERSION
 
 
+@pytest.mark.skipif(
+    sys.version_info < (3, 7), reason="capture_output is not supported in python 3.6"
+)
 def test_recorded_version_matches_git_tag():
     """Verify that we have the same version  in the package as in our git repository.
 
